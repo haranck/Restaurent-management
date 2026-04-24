03b7da7e5758288c32f6ae81f182ddf1c8ca7f41
Haranck <harankrishna03@gmail.com>
2026-04-24 14:52:50 +0530

    feat: initialize Prisma client with LibSQL adapter and singleton pattern support

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  return new PrismaClient({
+    adapter: new PrismaLibSql({
+      url: process.env.DATABASE_URL!,
+    }),
+  });
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
