e92ab398ded82f99c80d5d4b26b7c9c959fc5173
Haranck <harankrishna03@gmail.com>
2026-05-04 09:21:04 +0530

    final commit

--- backend/src/utils/hash.ts
+++ backend/src/utils/hash.ts
@@ -7,3 +7,16 @@ export const hashPassword = async (password:string) => {
 export const comparePassword = async (password:string,hash:string) => {
     return await bcrypt.compare(password,hash)
 }
+
+//   "scripts": {
+//     "dev": "nodemon server.ts",
+//     "build": "tsc",
+//     "start": "node dist/server.js",
+//     "db:migrate": "prisma migrate dev",
+//     "db:push": "prisma db push",
+//     "db:seed": "prisma db seed",
+//     "db:generate": "prisma generate",
+//     "db:dev": "prisma dev",
+//     "db:studio": "prisma studio",
+//     "dev:all": "concurrently \"npm run dev\" \"npm run db:studio\""
+//   },
